0bc8c75a574ca8082860139c5026a875f3e2b419
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-07-28 22:11:08 -0400

    final changes to make script work

--- js/questions.js
+++ js/questions.js
@@ -1,147 +1,53 @@
-var startButton = document.getElementById("startButton");
-var quizContainer = document.getElementById("quizContainer");
-var questionElement = document.getElementById("question");
-var choicesElement = document.getElementById("choices");
-var submitButton = document.getElementById("submitButton");
+var questionData = [
+    {
+        question: "Which of the following is NOT a programming language used for web development?",
+        choices: ["HTML", "CSS", "Javascript", "SQL", "All of the above are programming languages used in web development"],
+        answer: "All of the above are programming languages used in web development"
+    },
+    {
+        question: "What does CSS stand for?",
+        choices: ["Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets", "Colorful Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        question: " What does the 'DOCTYPE' declaration do in HTML?",
+        choices: ["Defines the character encoding for the HTML document", "Specifies the version of HTML in use", "Indicates the beginning of the HTML document", "Provides instructions for the web browser on how to render the page"],
+        answer: "Specifies the version of HTML in use"
+    },
+    {
+        question: "What is the purpose of a CSS reset?",
+        choices: ["To clear the browser cache", "To undo browser default styles and provide a consistent starting point", "To reset the server-side cache", "To reset the HTML structure of the page"],
+        answer: "To undo browser default styles and provide a consistent starting point"
+    },
+    {
+        question: "Which of the following is NOT a commonly used version control system?",
+        choices: ["Git", "Subversion (SVN)", "Mercurial", "Bitbucket"],
+        answer: "Bitbucket"
+    },
+    {
+        question: "What does the 'this' keyword refer to in JavaScript?",
+        choices: ["The current web page", "The previous element in the DOM", "The global object", "The object that called the function"],
+        answer: "The object that called the function"
+    },
+    {
+        question: "What is the purpose of AJAX in web development?",
+        choices: ["To create animated effects on the web page", "To enable asynchronous communication between the web browser and server", "To enhance search engine optimization (SEO)", "To optimize web page loading speed"],
+        answer: "To enable asynchronous communication between the web browser and server"
+    },
+    {
+        question: "What is a responsive web design?",
+        choices: ["A design that includes vibrant colors and eye-catching visuals", "A design that adapts and adjusts to different screen sizes and devices", "A design that uses JavaScript extensively for interactivity", "A design that follows modern design trends"],
+        answer: "A design that adapts and adjusts to different screen sizes and devices"
+    },
+    {
+        question: "What is the purpose of media queries in CSS?",
+        choices: ["To apply different styles based on the user's media consumption habits", "To target specific HTML elements for styling", "To conditionally apply styles based on the characteristics of the device or browser", "To embed media files directly into the web page"],
+        answer: "To conditionally apply styles based on the characteristics of the device or browser"
+    },
+    {
+        question: "What is the purpose of a favicon in web development?",
+        choices: ["To display the website's logo in the browser's address bar", "To improve website security", "To provide visual cues for the user during interactions", "To store user preferences and settings"],
+        answer: "To display the website's logo in the browser's address bar"
+    },
 
-var currentQuestionIndex = 0;
-var score = 0;
-
-var questions = [
-  {
-    question: "Question 1: What is the capital of France?",
-    choices: ["Paris", "London", "Madrid", "Rome"],
-    correctAnswer: 0
-  },
-  {
-    question: "Question 2: Which planet is known as the Red Planet?",
-    choices: ["Mars", "Venus", "Jupiter", "Mercury"],
-    correctAnswer: 0
-  },
-  // Add more questions here...
 ];
-
-function showQuestion() {
-  var question = questions[currentQuestionIndex];
-  questionElement.textContent = question.question;
-
-  choicesElement.innerHTML = "";
-  for (var i = 0; i < question.choices.length; i++) {
-    var choice = question.choices[i];
-    var li = document.createElement("li");
-    li.textContent = choice;
-    choicesElement.appendChild(li);
-  }
-}
-
-function checkAnswer() {
-  var selectedChoice = choicesElement.querySelector("li.selected");
-  if (selectedChoice) {
-    var selectedAnswer = Array.from(choicesElement.children).indexOf(selectedChoice);
-    if (selectedAnswer === questions[currentQuestionIndex].correctAnswer) {
-      score++;
-    }
-    selectedChoice.classList.remove("selected");
-    currentQuestionIndex++;
-    if (currentQuestionIndex === questions.length) {
-      endQuiz();
-    } else {
-      showQuestion();
-    }
-  }
-}
-
-function endQuiz() {
-  quizContainer.innerHTML = "<h2>Quiz Completed!</h2>" +
-    "<p>Your score: " + score + " out of " + questions.length + "</p>";
-}
-
-startButton.addEventListener("click", function() {
-  startButton.style.display = "none";
-  quizContainer.style.display = "block";
-  showQuestion();
-});
-
-choicesElement.addEventListener("click", function(event) {
-  if (event.target.nodeName === "LI") {
-    event.target.classList.toggle("selected");
-  }
-});
-
-submitButton.addEventListener("click", checkAnswer);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Questions supplied from Chat GPT
-
-// Which of the following is NOT a programming language used for web development?
-// a) HTML
-// b) CSS
-// c) JavaScript
-// d) SQL
-// e) All of the above are programming languages used in web development
-
-// What does CSS stand for?
-// a) Creative Style Sheets
-// b) Cascading Style Sheets
-// c) Computer Style Sheets
-// d) Colorful Style Sheets
-
-// What does the "DOCTYPE" declaration do in HTML?
-// a) Defines the character encoding for the HTML document
-// b) Specifies the version of HTML in use
-// c) Indicates the beginning of the HTML document
-// d) Provides instructions for the web browser on how to render the page
-
-// What is the purpose of a CSS reset?
-// a) To clear the browser cache
-// b) To undo browser default styles and provide a consistent starting point
-// c) To reset the server-side cache
-// d) To reset the HTML structure of the page
-
-// Which of the following is NOT a commonly used version control system?
-// a) Git
-// b) Subversion (SVN)
-// c) Mercurial
-// d) Bitbucket
-
-// What does the "this" keyword refer to in JavaScript?
-// a) The current web page
-// b) The previous element in the DOM
-// c) The global object
-// d) The object that called the function
-
-// What is the purpose of AJAX in web development?
-// a) To create animated effects on the web page
-// b) To enable asynchronous communication between the web browser and server
-// c) To enhance search engine optimization (SEO)
-// d) To optimize web page loading speed
-
-// What is a responsive web design?
-// a) A design that includes vibrant colors and eye-catching visuals
-// b) A design that adapts and adjusts to different screen sizes and devices
-// c) A design that uses JavaScript extensively for interactivity
-// d) A design that follows modern design trends
-
-// What is the purpose of media queries in CSS?
-// a) To apply different styles based on the user's media consumption habits
-// b) To target specific HTML elements for styling
-// c) To conditionally apply styles based on the characteristics of the device or browser
-// d) To embed media files directly into the web page
-
-// What is the purpose of a favicon in web development?
-// a) To display the website's logo in the browser's address bar
-// b) To improve website security
-// c) To provide visual cues for the user during interactions
-// d) To store user preferences and settings
--- js/script.js
+++ js/script.js
@@ -1,79 +1,120 @@
+var timerEl = document.querySelector('#timer');
+var body = document.body;
+var startBtnEl = document.querySelector('#start-btn');
+var gameTitle = document.querySelector('#start-screen h2');
+var quizScreen = document.querySelector('#quiz-wrap');
+var startScreen = document.querySelector('#start-screen');
+var questionText = document.querySelector('#question-display');
+var choicesEl = document.querySelector('.choices');
+var messageEl = document.querySelector('#feedback-message');
+var endScreen = document.querySelector('#end-screen')
+var resultText = document.querySelector('#end-screen h2');
+var endingInstructions = document.querySelector('#end-instructions')
+var initialsForm = document.querySelector('#initialsInput');
+var initialsInput = document.querySelector('#initials');
+var scoreScreen = document.querySelector('#score-screen');
+var currentQuestionIndex = 0;
+var time = 75;
+var timer;
+var totalCorrect = 0;
 
-// Get the timer element from the DOM
-var timerElement = document.getElementById("timer");
-var timerInterval;
 
-// Set the timer duration (in seconds)
-var duration = 60;
-var button = document.getElementById("button");
-
-// Function to format the time
-function formatTime(time) {
-    var minutes = Math.floor(time / 60);
-    var seconds = time % 60;
-    return minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+function startGame() {
+    startScreen.classList.add('hide');
+    quizScreen.classList.remove('hide');
+    timerEl.classList.remove('hide');
+    quizScreen.classList.add('flex');
+    timer = setInterval(countDown, 750);
+    countDown();
+    playGame();
 }
 
-// Function to update the timer
-function updateTimer() {
-    duration--;
-    timerElement.textContent = formatTime(duration);
+function countDown() {
+    time--;
+    timerEl.innerText = time;
 
-    if (duration <= 0) {
-        clearInterval(timerInterval);
-        alert("Time's up!");
+    if (time <= 0) {
+        clearInterval(timer);
+        endScreenShow();
     }
 }
 
-// Update the timer immediately
-// updateTimer();
-
-// Start the timer interval (updates every second)
-// var timerInterval = setInterval(updateTimer, 1000);
-
-
-
-// var button = document.querySelector('.btn')
-
-button.addEventListener('click', function () {
-    if (!timerInterval) {
-        timerInterval = setInterval(updateTimer, 1000);
+function playGame(){
+    if(currentQuestionIndex < questionData.length){
+    questionText.textContent = questionData[currentQuestionIndex].question;
+    choicesEl.innerHTML = '';
+    for(var i = 0; i<questionData[currentQuestionIndex].choices.length; i++){
+        var newBtn = document.createElement('button');
+        newBtn.textContent = questionData[currentQuestionIndex].choices[i];
+        newBtn.addEventListener('click', nextQuestion)
+        choicesEl.append(newBtn);
     }
-});
+}else{
+    endScreenShow()
+
+}
+}
+
+function nextQuestion(event){
+    var userChoice = event.target.textContent;
+    var correctChoice = questionData[currentQuestionIndex].answer;
+    if (userChoice === correctChoice){
+        totalCorrect++;
+        messageEl.textContent = 'Correct!';
+    }else{
+        messageEl.textContent = 'Wrong';
+        time-=10;
+    }
+    setTimeout(function(){
+        messageEl.textContent = ''
+    }, 2000)
+    currentQuestionIndex++;
+    playGame();
+}
 
 
+function endScreenShow(){
+   
+    resultText.textContent = 'Your score is ' + totalCorrect
+    quizScreen.classList.add('hide');
+    initialsForm.classList.remove('hide');
+    endScreen.classList.remove('hide');
+    timerEl.classList.add('hide');
+    clearInterval(timer);
+}
 
+function getlocalStorage (){
+    var rawData = localStorage.getItem('savedScores');
+    var parsed = JSON.parse(rawData);
+    return parsed || [];
+}
 
+function addScore(event){
+    event.preventDefault();
+    var scores = getlocalStorage();
+    var newScore =  {
+        name: initialsInput.value,
+        score: totalCorrect
+    }
 
+    scores.push(newScore);
+    localStorage.setItem('savedScores', JSON.stringify(scores));
+    console.log(scores);
+    displayScores();
 
+}
 
+function displayScores(){
+    endScreen.classList.add('hide')
+    scoreScreen.classList.remove('hide');
+    var parsed = JSON.parse(localStorage.getItem("savedScores"));
+    console.log(parsed);
+    for (var i =0; i < parsed.length; i++){
+        var currentScore = document.createElement('p')
+        currentScore.textContent = 'Name: ' + parsed[i].name + ' Score: ' + parsed[i].score;
+        scoreScreen.append(currentScore);
+    }
+}
 
-
-
-
-
-
-// function beginQuiz(event) {
-//     var btnOne = event.target;
-
-//     console.log(click)
-// }
-
-
-
-
-// function startTimer() {
-//     timer = setInterval(function () {
-//         time--;
-//         timer.innerText = `Time:` + time;
-
-//         if (time <= 0) {
-//             clearInterval(timer);
-//             timerElement.textContent = "Countdown Finished!"
-//         }
-//     }, 1000);
-// }
-// startTimer();
-
-
-
+startBtnEl.addEventListener('click', startGame);
+initialsForm.addEventListener("submit", addScore);
